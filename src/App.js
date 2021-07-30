811e4b0222b379e9084439bb815e598efcdbb9a1
Van Thanh <thanhokbicaso@gmail.com>
2021-07-30 15:45:54 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Le Van Thanh
+          Le Van Thanh :))
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
